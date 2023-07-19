c283c64e077d5beea52fcb6f0921b6b1dae7a0c3
OblacneCerealie <bastymichalko@gmail.com>
2023-07-19 13:05:02 +0200

    Starting dicee challenge

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,1 +1,4 @@
-document.query
+document.querySelector("ul a" ).style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("button").style.border = "none";
+document.querySelector("h1").classList.add ("huge");
