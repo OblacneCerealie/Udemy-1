e21984742fe7b7c7f7ea4dd8d0232f2f93ce7dcf
OblacneCerealie <bastymichalko@gmail.com>
2023-09-27 17:37:48 +0200

    roject TODOLIST started.
    issues

--- Backend/TODO_CAPS_project/index.js
+++ Backend/TODO_CAPS_project/index.js
@@ -1,12 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+
+
+
 const app = express();
 const port = 3000;
 
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+
+
+app.post("/submit", (req, res) => {
+
+    let myActivity = req.body["activity"    ];
+
+
+    let AllA = [];
+    AllA.push(myActivity);
+
+
+    
+    
+    
+    res.render("index.ejs", {
+    });
+    });
+
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+    });
 
 
 
