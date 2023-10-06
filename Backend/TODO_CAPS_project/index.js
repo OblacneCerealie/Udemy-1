50020357c9e1ef758070b767fc0d5fdb6ee33347
OblacneCerealie <bastymichalko@gmail.com>
2023-10-06 16:36:15 +0200

    I officially give up on this project, never working with ejs ever again, I hate node.

--- Backend/TODO_CAPS_project/index.js
+++ Backend/TODO_CAPS_project/index.js
@@ -1,69 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
-import ejs from "ejs";
-import path from "path";
-
-
-
 
 const app = express();
 const port = 3000;
-
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let AllA = [];
 
-
 app.get("/", (req, res) => {
     res.render("index.ejs");
-
-    });
-
-
+});
 
 app.post("/submit", (req, res) => {
-
     let myActivity = req.body["activity"];
-
-
-
     AllA.push(myActivity);
-    res.redirect("/")
-
-
-    
-    
-    
-    res.render("index.ejs", {
-        activitiesL: AllA
-
-    });
-    }); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    res.redirect("/");
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-    });
+});
+
+
+
+
+
+
+
+
+
+
